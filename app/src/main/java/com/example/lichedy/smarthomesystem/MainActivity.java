c951ae2f31a4a826691a1a362a661418cd8675fd
Mateo Rotim <lichedy@gmail.com>
2016-06-02 00:42:11 +0200

    Added dialog for creating alarm when pressed on floating action button, created custom layout for that dialog, added sending encrypted json string when pressed on positive button of dialog.

--- app/src/main/java/com/example/lichedy/smarthomesystem/MainActivity.java
+++ app/src/main/java/com/example/lichedy/smarthomesystem/MainActivity.java
@@ -1,17 +1,28 @@
 package com.example.lichedy.smarthomesystem;
 
+import android.app.Activity;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.view.ContextThemeWrapper;
+import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.inputmethod.InputMethodManager;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.NumberPicker;
+import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.gson.Gson;
@@ -24,7 +35,10 @@ import cz.msebera.android.httpclient.Header;
 import cz.msebera.android.httpclient.entity.StringEntity;
 
 public class MainActivity extends AppCompatActivity {
-    String ipAdress="192.168.1.117:3000";
+    String ipAdress;
+    int counter = 0;
+    Spinner vSpinner;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,18 +47,13 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
         ipAdress = PreferenceManager.getDefaultSharedPreferences(getApplicationContext()).getString("ip adress", "192.168.1.117:3000");
+
+        final SharedPreferences counterPreference = getSharedPreferences("counter number", Activity.MODE_PRIVATE);
+
         final EditText messageBox = (EditText)findViewById(R.id.messageBox);
         final EditText ipBox = (EditText)findViewById(R.id.ipBox);
+        assert ipBox != null;
         ipBox.setText(ipAdress);
         Button sendMsg = (Button)findViewById(R.id.button);
 
@@ -59,6 +68,11 @@ public class MainActivity extends AppCompatActivity {
                     System.out.println("input=" + input);
                     String decrypted = decrypt(message,iv);
                     Toast.makeText(getApplicationContext(),decrypted,Toast.LENGTH_SHORT).show();
+                    if(decrypted == "OK"){
+                        SharedPreferences.Editor editor = counterPreference.edit();
+                        editor.putInt("counter number", counter+1);
+                        editor.commit();
+                    }
                 } catch (UnsupportedEncodingException e) {
                     e.printStackTrace();
                 } catch (Exception e) {
@@ -85,6 +99,8 @@ public class MainActivity extends AppCompatActivity {
             }
         };
 
+        //////////////THIS IS ONLY FOR TEST PURPOSES/////////////////
+        assert sendMsg != null;
         sendMsg.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -101,14 +117,96 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+        ///////////////////////////////////////////////////////////
 
-        try {
-            String crypted = encrypt("stipe");
-            StringEntity sEntity = new StringEntity(crypted,"UTF-8");
-            client.post(ipAdress,response,sEntity);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        final AlertDialog.Builder builder = new AlertDialog.Builder(new ContextThemeWrapper(this,R.style.AppTheme_Dialog));
+        LayoutInflater inflater = getLayoutInflater();
+        final View dialogView = inflater.inflate(R.layout.create_alarm_dialog,null);
+        final AlertDialog alarmDialog = builder.create();
+
+
+        alarmDialog.setView(dialogView);
+        alarmDialog.setContentView(R.layout.content_main);
+
+        final ArrayAdapter<CharSequence> valveAdapter = ArrayAdapter.createFromResource(this, R.array.valves, R.layout.support_simple_spinner_dropdown_item);
+        valveAdapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
+
+        vSpinner = (Spinner)dialogView.findViewById(R.id.valveSpinner);
+        vSpinner.setAdapter(valveAdapter);
+
+        final TextView alarmName = (TextView)dialogView.findViewById(R.id.alarmNameBox);
+
+        final NumberPicker npHour = (NumberPicker)dialogView.findViewById(R.id.hourNP);
+        final NumberPicker npMinute = (NumberPicker)dialogView.findViewById(R.id.minuteNP);
+        final NumberPicker npDuration = (NumberPicker)dialogView.findViewById(R.id.durationNP);
+        npHour.setMaxValue(23);
+        npHour.setMinValue(0);
+        npMinute.setMaxValue(23);
+        npMinute.setMinValue(0);
+        npDuration.setMaxValue(23);
+        npDuration.setMinValue(0);
+
+        NumberPicker.Formatter nf = new NumberPicker.Formatter() {
+            @Override
+            public String format(int value) {
+                return String.format("%02d", value);
+            }
+        };
+        npHour.setFormatter(nf);
+        npMinute.setFormatter(nf);
+        npDuration.setFormatter(nf);
+
+        alarmDialog.setButton(DialogInterface.BUTTON_POSITIVE, "Create", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+
+                Message alarm = new Message();
+                alarm.command = "1";
+                alarm.device = "";
+                alarm.counter = counterPreference.getInt("counter number", -1);
+                alarm.timer.name = alarmName.getText().toString();
+                alarm.timer.device = vSpinner.getSelectedItem().toString();
+                alarm.timer.startTime = calculateStartTime(npHour.getValue(),npMinute.getValue());
+                alarm.timer.endTime = calculateEndTime(npHour.getValue(),npMinute.getValue(),npDuration.getValue());
+                alarm.timer.active = true;
+                alarm.timer.enabled = true;
+
+                try {
+                    String encrypted = encrypt(jsonStringify(alarm));
+                    StringEntity sEntity = new StringEntity(encrypted,"UTF-8");
+                    client.post(ipAdress,response,sEntity);
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+            }
+        });
+
+        alarmDialog.setButton(DialogInterface.BUTTON_NEGATIVE, "Cancle", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+
+
+
+
+
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        assert fab != null;
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                alarmDialog.show();
+
+                //Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG).setAction("Action", null).show();
+            }
+        });
     }
 
     @Override
@@ -162,7 +260,7 @@ public class MainActivity extends AppCompatActivity {
         System.out.println("key=" + key);
         String output = _crypt.encrypt(message, key,iv); //decrypt
         System.out.println("iv=" + iv);
-        System.out.println("rest" + output);
+        System.out.println("message " + output);
         return iv + output;
     }
 
@@ -194,5 +292,13 @@ public class MainActivity extends AppCompatActivity {
         public Timer(){
             super();
         }
+    }
+
+    public int calculateStartTime(int hours, int minutes){
+        return hours * 60 * 60 + minutes * 60;
+    }
+
+    public int calculateEndTime(int hours, int minutes, int duration){
+        return hours * 60 * 60 + minutes * 60 + duration * 60;
     }
 }
